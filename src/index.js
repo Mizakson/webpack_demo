69d80795bc7049d3e96a6824fb3d136515348a31
Mizakson <maxwellizakson@gmail.com>
2024-01-26 11:03:58 -0500

    Import style.css in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-// import _ from 'lodash';
-import myName from './myName';
+import _ from 'lodash';
+// import myName from './myName';
+
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash is required for this line to work
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-    element.textContent = myName('Max');
+    // element.textContent = myName('Max');
 
     return element;
 }
