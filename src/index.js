3bbc649eed9e0be95c78d3a7a44c56108877444e
Mizakson <maxwellizakson@gmail.com>
2024-01-29 10:12:42 -0500

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -17,6 +19,9 @@ function component() {
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
